2b708690bf16baad1cebe5336b40a236a69decf8
Alireza Adabi <alireza.adabi78@gmail.com>
2022-08-31 19:52:39 +0430

    add: Navbar

---
+++ pages/factory/resources.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const Resources = () => {
+  return (
+    <div className="w-full">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Resources
---
+++ pages/factory/sensors.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const Sensors = () => {
+  return (
+    <div className="w-full">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Sensors;
